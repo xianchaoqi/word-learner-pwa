db4ac12b05bacf066991eb07ec40530b17a835f2
xianchaoqi <xdwincom00@gmail.com>
2026-05-14 02:45:10 +0800

    Publish word learner PWA

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "word-learner-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./app-icon.svg",
+  "./app-icon-180.png",
+  "./app-icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
